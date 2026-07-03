56250db352ed91989c43a495a363978a47def5e7
TheCodeofSP <thecodeofsp@gmail.com>
2026-07-03 02:29:03 +0200

    fix: correct legal component import

--- apps/web/src/pages/Legal/LegalNotice.jsx
+++ apps/web/src/pages/Legal/LegalNotice.jsx
@@ -1,6 +1,5 @@
-import LegalPage from "../../components/Legal/LegalPage.jsx";
+import LegalPage from "../../components/legal/LegalPage.jsx";
 import { legalContent } from "../../content/legal.content.js";
-
 export default function LegalNotice() {
   return <LegalPage {...legalContent.legalNotice} />;
 }
